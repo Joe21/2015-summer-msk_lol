82c28c794814f4afa5664304678127a52343608a
Joe <joej21@gmail.com>
2015-07-28 18:57:16 -0400

    finish implementing fb oauth session and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'sessions#index'
+
+  get 'auth/:provider/callback' => 'sessions#create'
+  get 'auth/failure' => redirect('/')
+  get 'signout' => 'sessions#destroy', as: 'signout'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
