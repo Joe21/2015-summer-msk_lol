088c9336a7c199f3bfdbe49e58ee5983c2ac0285
Joe <joej21@gmail.com>
2015-08-04 15:26:06 -0400

    formulating basic img load and design for form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,7 +18,9 @@ class GamesController < ApplicationController
 		# Randomly select 9 champs
 		@round.initialize_participants
 
-		@champs = @round.champs
+		@champs1 = @round.champs[0..2]
+		@champs2 = @round.champs[3..5]
+		@champs3 = @round.champs[6..8]
 		# # Convert to array
 		# array_of_champs = new_round.champs.map { |champ| champ }
 		
